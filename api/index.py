a9e02aea64af75c9b173c184f85fde73b5275220
bltsushi6-cpu <bltsushi6@gmail.com>
2026-07-29 07:51:29 -0700

    Finalied the At-Home symptom checker

--- api/index.py
+++ api/index.py
@@ -818,4 +818,4 @@ def generate_mock_vision(phase):
         }
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=5000, debug=True)
